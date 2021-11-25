28104ac8dc4cd4d84a6cbbf1852fa1a51fce96b6
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:50:15 +0000

    ummmm

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,8 +1,27 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
-
-    return NO_MATCH;
+    var mid = items.length / 2; //var gang
+    var done = false;
+    while(done == false) {
+        if(item != items[mid]) {
+            if(item > items[mid]) {
+                items = items.slice(mid);
+                mid = items.length / 2;
+            }
+            else if(item < items[mid]) {
+                items = items.slice(-mid);
+                mid = items.length / 2;
+            }
+        }
+        else if(item == items[mid]){
+            done = true;
+        }
+        else {
+            done = true
+            return NO_MATCH;
+        }
+    }
 }
 
 export default binarySearch;
